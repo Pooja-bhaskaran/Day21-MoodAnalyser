366225a651672fc0f2eb8ed32cabcc82ddfa708e
unknown <https://github.com/Pooja-bhaskaran/Day03.git>
2022-07-10 01:34:55 +0530

    UC1.1 Sad analyse is added

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -1,7 +1,17 @@
 public class MoodAnalyser {
 
-    public static String analyzeMood(String message) {
+    protected static String analyzeMood(String message) {
         if (message.contains("sad")) {
+            return "SAD";
+        } else if (message.contains("Happy")) {
+            return "HAPPY";
+        } else {
+            return "null";
+        }
+    }
+
+    protected static String analyzeMood1(String message) {
+        if (message.contains("I am in sad mood")) {
             return "SAD";
         } else if (message.contains("Happy")) {
             return "HAPPY";
